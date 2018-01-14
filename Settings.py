aef82fe8b21beab46de3cce810786269d43988ea
MrAnderson91 <klodale9491@gmail.com>
2018-01-14 15:46:18 +0100

    Modifica Settings

--- Settings.py
+++ Settings.py
@@ -1,3 +1,3 @@
-MyEncryptorServerAddress = "http://80.211.233.128/MyEncryptorServer/"
+MyEncryptorServerAddress = "http://myserver/mywebservicepage"
 PasswordHashFunction = "SHA256"
 EncryptedExtension = '.enc'
